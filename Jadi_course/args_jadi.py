b61d20fe6f0e1b5019e25611eeec052ac49e2fcc
Mehran Fallah <M.Mehran.Fallah@gmail.com>
2025-07-04 20:11:03 +0330

    complete args exercise

--- Jadi_course/args_jadi.py
+++ Jadi_course/args_jadi.py
@@ -1,3 +1,4 @@
+# Project 1
 def sum_numbers(*num):
     if num == 0:
         return 0
@@ -6,7 +7,7 @@ def sum_numbers(*num):
         res += i
     return res
 
-
+# Project 2
 def pick_evens(*args):
     l = []
     if args == 0:
@@ -16,6 +17,11 @@ def pick_evens(*args):
             l.append(i)
     return l
 
+# Project 3
+def skyline(*args):
+    return 0 if not args else max(args)
 
-print(sum_numbers(1,2,3))
-print(pick_evens(1,2,3,4,5))
+
+print(skyline())
+print(sum_numbers(1, 2, 3))
+print(pick_evens(1, 2, 3, 4, 5))
