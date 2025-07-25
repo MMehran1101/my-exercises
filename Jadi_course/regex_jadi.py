8cab59de95a3306474bcd1508fdaf8695aaed400
Mehran Fallah <M.Mehran.Fallah@gmail.com>
2025-07-25 16:00:17 +0330

    Update regex_jadi.py

--- Jadi_course/regex_jadi.py
+++ Jadi_course/regex_jadi.py
@@ -19,14 +19,33 @@ import re
 # 2. Find emails
 
 emails = "برای تماس: ali@gmail.com، sara_2020@yahoo.com و test-user@outlook.com و mehran@sds2.cs"
+# emails = "برای تماس: ali@gmail.com، sara_2020@yahoo.com و test-user@outlook.com و mehran@sds2.cs"
 
 email_pattern = r"[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}"
+# email_pattern = r"[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}"
 
 result = re.finditer(email_pattern, emails)
+# result = re.finditer(email_pattern, emails)
+
+# for i in result:
+#     print(i.group())
+
+# 2. End
+
+# 3. English Word from 4 to 6 letters
+
+data = "We are learning regex with python. Easy? Maybe, maybe not!"
+
+eng_pattern = r"[a-zA-Z]{4,6}"
+
+result = re.finditer(eng_pattern, data)
 
 for i in result:
     print(i.group())
 
 # 1. End
 # 2. End
+# 3. End
+
+
 
