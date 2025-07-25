0358ad49d4dfa2829ef18e6e0d68b127ef8c0529
Mehran Fallah <M.Mehran.Fallah@gmail.com>
2025-07-25 15:59:34 +0330

    find-emails

--- Jadi_course/regex_jadi.py
+++ Jadi_course/regex_jadi.py
@@ -2,15 +2,31 @@ import re
 
 # 1. Find Iranian phone number
 
-data = "شماره من 09121234567 هست ولی قبلاً 09351234567 داشتم."
+# data = "شماره من 09121234567 هست ولی قبلاً 09351234567 داشتم."
 
-phone_pattern = r"09\d{9}"
+# phone_pattern = r"09\d{9}"
 
 
-result = re.finditer(phone_pattern, data)
+# result = re.finditer(phone_pattern, data)
 
+
+# for i in result:
+#     print(i.group())
+
+# 1. End
+
+
+# 2. Find emails
+
+emails = "برای تماس: ali@gmail.com، sara_2020@yahoo.com و test-user@outlook.com و mehran@sds2.cs"
+
+email_pattern = r"[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}"
+
+result = re.finditer(email_pattern, emails)
 
 for i in result:
     print(i.group())
 
 # 1. End
+# 2. End
+
